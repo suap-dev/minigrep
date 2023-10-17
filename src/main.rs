3301cd7a7b93fda84a1fff3190f4dad867357eaa
suap-dev <suap.dev@gmail.com>
2023-10-18 00:05:35 +0200

    Add parse_config fn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+    // Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
+    // As long as your command line parsing logic is small, it can remain in main.rs.
+    // When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
+    
 
-    let query = &args[1];
-    let file_path = &args[2];
+fn main() {
+    let args: Vec<String> = env::args().collect();    
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {query}");
     println!("In file {file_path}");
@@ -13,4 +16,12 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // ignoring args[0] - file path
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
