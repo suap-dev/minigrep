3ae417d5e0c1b546f225b9348e2efed38a867827
suap-dev <suap.dev@gmail.com>
2023-10-18 00:12:29 +0200

    Add Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use std::env;
 use std::fs;
 
-    // Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
-    // As long as your command line parsing logic is small, it can remain in main.rs.
-    // When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
-    
+// Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
+// As long as your command line parsing logic is small, it can remain in main.rs.
+// When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
 
 fn main() {
-    let args: Vec<String> = env::args().collect();    
-    let (query, file_path) = parse_config(&args);
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    // ignoring args[0] - file path
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    // ignoring args[0] - file path
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
