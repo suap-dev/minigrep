01d318d3e6aa581e2ee1c8a8f006cca4b261afa5
suap-dev <suap.dev@gmail.com>
2023-10-18 03:43:24 +0200

    Separate the logic from main fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::nursery, clippy::pedantic)]
 
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -10,6 +11,7 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
@@ -18,10 +20,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(err) = run(config) {
+        println!("Application error: {err}");
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
