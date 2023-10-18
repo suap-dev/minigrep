731a14d069dc95c7c142292eff0ed593cb226ff7
suap-dev <suap.dev@gmail.com>
2023-10-18 03:50:14 +0200

    Move logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 #![warn(clippy::all, clippy::nursery, clippy::pedantic)]
 
-use std::env;
-use std::error::Error;
-use std::fs;
-use std::process;
+use std::{env, process};
 
-// Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
-// As long as your command line parsing logic is small, it can remain in main.rs.
-// When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,33 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         println!("Application error: {err}");
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
-    }
 }
