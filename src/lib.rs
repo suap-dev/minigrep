5aca0108e18fa37f2f57f0304aeae4172b3779ff
suap-dev <suap.dev@gmail.com>
2023-10-20 01:38:34 +0200

    search fn passes one_result test fn.

--- src/lib.rs
+++ src/lib.rs
@@ -28,15 +28,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // Refactor the code you just added or changed and make sure the tests continue to pass.
 // Repeat from step 1!
 
-
-
-fn search(query: &str, contents: &str) -> Vec<String> {
-    vec!["no".to_owned()]
-}
-
-// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-//     vec![]
+// fn search(query: &str, contents: &str) -> Vec<String> {
+//     for line in contents.lines() {
+//         if line.contains(query) {}
+//     }
+//     vec!["no".to_owned()]
 // }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 #[cfg(test)]
 mod tests {
