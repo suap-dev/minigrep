e235dc8cd48a10ce082f686f9a5084f8bb89f006
suap-dev <suap.dev@gmail.com>
2023-10-21 21:27:07 +0200

    println->eprintln

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,12 @@ fn main() {
     let ignore_case = env::var("IGNORE_CASE").is_ok();
 
     let config = Config::build(&args, ignore_case).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     };
 }
